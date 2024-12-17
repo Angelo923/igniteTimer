3b19fbdaa9ecf0fa67c8c072d1a3919ffef69790
Angelo <angelo-marcos@hotmail.com>
2024-12-16 21:38:09 -0300

    Criado o componente Header para ficar fixo na pagina e feito sua estilização e navegação

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import {HeaderContainer} from "./styles.ts";
+import igniteLogo from '../../assets/Logo.svg'
+import {Scroll, Timer} from "phosphor-react";
+import {NavLink} from "react-router-dom";
+
+function Header() {
+    return (
+        <HeaderContainer>
+            <span>
+                <img
+                    src={igniteLogo}
+                    alt="Logo do Ignite, dois triangulos seguidos a 45 graus na cor verde" />
+            </span>
+            <nav>
+                <NavLink
+                    to="/"
+                    title="Timer"
+                >
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink
+                    to="/history"
+                    title="History"
+                >
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
+
+export default Header;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route} from 'react-router-dom'
 import Home from "../pages/Home.tsx";
 import History from "../pages/History.tsx";
+import Index from "../layouts/DefaultLayout";
 
 function Router() {
     return (
         <Routes>
+          <Route path="/" element={<Index />}>
             <Route path="/" element={<Home />} />
             <Route path="/history" element={<History />} />
+          </Route>
         </Routes>
     )
 }
